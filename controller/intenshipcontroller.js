6d9fc29f221e8c21f613da7dd75f8be2956ad25d
nandakumar999 <tnandakumar@digispheretech.in>
2026-02-25 12:50:18 +0530

    date has changed

--- controller/intenshipcontroller.js
+++ controller/intenshipcontroller.js
@@ -1,40 +1,40 @@
-const pool=require('../config/db');
+const pool = require('../config/db');
 
 
 exports.addinternship = async (req, res) => {
-    try {
-        const { student_id, project_name, github_url, description,web_url } = req.body;
-        
-        
+  try {
+    const { student_id, project_name, github_url, description, web_url } = req.body;
 
-     
-        await pool.query(`
+
+
+
+    await pool.query(`
             INSERT INTO tbl_internship 
             (student_id, project_name, github_url, web_url, description, applied_date) 
             VALUES ($1, $2, $3, $4, $5, NOW())
         `,
-        [student_id, project_name, github_url, web_url, description]);
+      [student_id, project_name, github_url, web_url, description]);
 
-        return res.status(200).json({
-            statusCode: 200,
-            message: "Submitted Successfully"
-        });
+    return res.status(200).json({
+      statusCode: 200,
+      message: "Submitted Successfully"
+    });
 
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            statusCode: 500,
-            message: "Internal Server Error"
-        });
-    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      statusCode: 500,
+      message: "Internal Server Error"
+    });
+  }
 };
 
 exports.getinternship = async (req, res) => {
-    const { user_id } = req.body;
+  const { user_id } = req.body;
 
-    try {
-        const result = await pool.query(
-            `SELECT 
+  try {
+    const result = await pool.query(
+      `SELECT 
                 u.full_name,
                 u.email,
                 i.project_name,
@@ -42,37 +42,37 @@ exports.getinternship = async (req, res) => {
                 i.web_url,
                 i.description,
                 i.status,
-                i.start_date,
-                i.end_date,
-                 TO_CHAR(i.applied_date, 'DD-MM-YYYY') AS applied_date
+                TO_CHAR(i.start_date, 'DD-MM-YYYY') AS start_date,
+                TO_CHAR(i.end_date, 'DD-MM-YYYY') AS end_date,
+                TO_CHAR(i.applied_date, 'DD-MM-YYYY') AS applied_date
             FROM tbl_user u
             LEFT JOIN tbl_internship i 
                 ON u.user_id = i.student_id
             WHERE u.user_id = $1
             ORDER BY i.internship_id DESC`,
-            [user_id]
-        );
+      [user_id]
+    );
 
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                statusCode: 404,
-                message: "No data found"
-            });
-        }
-
-        return res.status(200).json({
-            statusCode: 200,
-            message: "Fetched Successfully",
-            data: result.rows[0]
-        });
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            statusCode: 500,
-            message: "Internal Server Error"
-        });
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: "No data found"
+      });
     }
+
+    return res.status(200).json({
+      statusCode: 200,
+      message: "Fetched Successfully",
+      data: result.rows[0]
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      statusCode: 500,
+      message: "Internal Server Error"
+    });
+  }
 };
 
 
@@ -89,9 +89,9 @@ exports.gettotalinternship = async (req, res) => {
           i.web_url,
           i.description,
           i.status,
-          i.start_date,
-          i.end_date,
-           TO_CHAR(i.applied_date, 'DD-MM-YYYY') AS applied_date
+          TO_CHAR(i.start_date, 'DD-MM-YYYY') AS start_date,
+          TO_CHAR(i.end_date, 'DD-MM-YYYY') AS end_date,
+          TO_CHAR(i.applied_date, 'DD-MM-YYYY') AS applied_date
        FROM tbl_user u
        INNER JOIN tbl_internship i 
             ON u.user_id = i.student_id
