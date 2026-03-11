0aa0e5c72d3aa2f2dc0964daddbd197e1013e0f5
nandakumar999 <tnandakumar@digispheretech.in>
2026-03-11 11:13:25 +0530

    done changes

--- routes/tutorroutes.js
+++ routes/tutorroutes.js
@@ -4,13 +4,14 @@ const tutorcontroller = require('../controller/tutorcontroller');
 const uploads = require('../utils/uploadfile');
 const { verifyToken } = require("../middleware/authMiddleware");
 const { allowRoles } = require("../middleware/roleMiddleware");
+const {validateCertificateFiles} =require("../utils/validatefiles");
+const {validateProfilePic} =require('../utils/validatefiles');
 
-
-router.post('/addtutorabout', verifyToken, allowRoles("admin", "student", "tutor"), uploads.fields([
+router.post('/addtutorabout', verifyToken, allowRoles("admin", "student", "tutor"), validateProfilePic,uploads.fields([
   { name: 'profile_pic', maxCount: 10 },
 
 ]), tutorcontroller.addtutorAbout);
-router.post('/updatetutorprofile', verifyToken, allowRoles("admin", "student", "tutor"), uploads.fields([
+router.post('/updatetutorprofile', verifyToken, allowRoles("admin", "student", "tutor"), validateProfilePic,uploads.fields([
   { name: 'profile_pic', maxCount: 10 },
 ]), tutorcontroller.updateTutorProfilePic);
 
@@ -19,8 +20,8 @@ router.post('/updateTutorProfessionalDetails',verifyToken, allowRoles("admin", "
 
 router.post('/getprofilepic', verifyToken, allowRoles("admin", "student", "tutor"), tutorcontroller.getTutorProfilePic);
 router.post('/gettutoronbardbyuserid', verifyToken, allowRoles("admin", "student", "tutor"), tutorcontroller.getTutorOnboarding);
-router.post('/addtutorcertificates', verifyToken, allowRoles("admin", "student", "tutor"), uploads.fields([{ name: "certificate_file", maxCount: 20 }]), tutorcontroller.addCertificates);
-router.post('/updatetutorcertificates', verifyToken, allowRoles("admin", "student", "tutor"), uploads.any(), tutorcontroller.updateTutorCertificates);
+router.post('/addtutorcertificates', verifyToken, allowRoles("admin", "student", "tutor"), uploads.fields([{ name: "certificate_file", maxCount: 20 }]),validateCertificateFiles, tutorcontroller.addCertificates);
+router.post('/updatetutorcertificates', verifyToken, allowRoles("admin", "student", "tutor"), uploads.any(), validateCertificateFiles,tutorcontroller.updateTutorCertificates);
 router.post('/addtutoreducation', verifyToken, allowRoles("admin", "student", "tutor"), tutorcontroller.addEducation);
 router.post('/updatetutoreducation', verifyToken, allowRoles("admin", "student", "tutor"), tutorcontroller.updateEducation);
 
---
+++ utils/validatefiles.js
@@ -0,0 +1,72 @@
+exports.validateCertificateFiles = (req, res, next) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/jpg",
+    "application/pdf"
+  ];
+
+const maxSize = 10 * 1024 * 1024; // 10MB
+
+  if (!req.files || !req.files.certificate_file) {
+    return res.status(400).json({
+      message: "Certificate files are required"
+    });
+  }
+
+  const files = req.files.certificate_file;
+
+  for (let file of files) {
+    if (!allowedTypes.includes(file.mimetype)) {
+      return res.status(400).json({
+        statusCode:400,
+        message: "Only image and PDF files are allowed for certificates"
+      });
+    }
+
+    if (file.size > maxSize) {
+      return res.status(400).json({
+        statusCode:400,
+        message: "Certificate file size should not exceed 10MB"
+      });
+    }
+  }
+
+  next();
+};
+
+
+exports.validateProfilePic = (req, res, next) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/jpg"
+  ];
+
+  const maxSize = 100 * 1024; // 100KB
+
+  if (!req.files || !req.files.profile_pic) {
+    return res.status(400).json({
+        statusCode:400,
+      message: "Profile picture is required"
+    });
+  }
+
+  const file = req.files.profile_pic[0];
+
+  if (!allowedTypes.includes(file.mimetype)) {
+    return res.status(400).json({
+        statusCode:400,
+      message: "Only JPG, JPEG, PNG images are allowed for profile picture"
+    });
+  }
+
+  if (file.size > maxSize) {
+    return res.status(400).json({
+        statusCode:400,
+      message: "Profile picture size should not exceed 100KB"
+    });
+  }
+
+  next();
+};
