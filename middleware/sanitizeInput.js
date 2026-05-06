a1e6fcdea8b2af80449b525a73583963b12b1818
nandakumar999 <tnandakumar@digispheretech.in>
2026-05-06 10:25:30 +0530

    done

--- middleware/sanitizeInput.js
+++ middleware/sanitizeInput.js
@@ -1,15 +1,39 @@
+// const sanitizeInput = (req, res, next) => {
+//   const sanitize = (obj) => {
+//     for (let key in obj) {
+//       if (typeof obj[key] === "string") {
+//         obj[key] = obj[key].trim();
+//       }
+//     }
+//   };
+
+//   if (req.body) sanitize(req.body);
+//   if (req.query) sanitize(req.query);
+//   if (req.params) sanitize(req.params);
+
+//   next();
+// };
+
+// module.exports = sanitizeInput;
+
+
+
 const sanitizeInput = (req, res, next) => {
   const sanitize = (obj) => {
-    for (let key in obj) {
+    if (!obj) return;
+
+    Object.keys(obj).forEach((key) => {
       if (typeof obj[key] === "string") {
         obj[key] = obj[key].trim();
       }
-    }
+    });
   };
 
-  if (req.body) sanitize(req.body);
-  if (req.query) sanitize(req.query);
-  if (req.params) sanitize(req.params);
+  sanitize(req.body);
+  sanitize(req.query);
+  sanitize(req.params);
+
+  console.log("Sanitized Body:", req.body);
 
   next();
 };
